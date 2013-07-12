1b51737d02f99ddfe707d5249346ad8c21b80c6e
Ubuntu <ubuntu@ip-172-31-36-233.us-west-2.compute.internal>
2013-07-12 05:33:58 +0000

    commented readfile line

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = reqire('fs');
 var app = express.createServer(express.logger());
 
 
-fs.readFileSync('index.html');
+// fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
